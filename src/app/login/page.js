d2ac9e61028c9d148dd737254b5f0d1e4ff257a9
varunvaatsalya <a.varungupta23@gmail.com>
2024-10-14 23:27:36 +0530

    change

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import LoginComponent from "../components/LoginComponent";
 
-export default function Page() {
+function Page() {
   
   return (
     <div className=" flex flex-col min-h-screen bg-white">
@@ -26,3 +25,5 @@ export default function Page() {
     </div>
   );
 }
+
+export default Page;
