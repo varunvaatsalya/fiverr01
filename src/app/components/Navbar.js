105b01577c307d9b06b9ede8cff9efa6bf24bb55
varunvaatsalya <a.varungupta23@gmail.com>
2024-11-23 16:26:41 +0530

    navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -4,7 +4,7 @@ export default async function Navbar({ route }) {
   return (
     <header className="bg-slate-900 text-white p-4 sm:px-6 lg:px-8">
       <Link href={'./'} className="container mx-auto flex items-center lg:text-2xl font-bold hover:text-gray-300">
-        <div>Company</div>
+        <div>Shivam Akshayvat</div>
         {route?.map((name, index) => {
           return (
             <>
