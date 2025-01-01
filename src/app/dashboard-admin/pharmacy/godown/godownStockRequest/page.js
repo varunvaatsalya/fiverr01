cd3e0f15b08969fd12bf6fec94d1f2e55b06c468
varunvaatsalya <a.varungupta23@gmail.com>
2025-01-01 20:31:59 +0530

    resolve godown stock file bug

--- src/app/dashboard-admin/pharmacy/godown/godownStockRequest/page.js
+++ src/app/dashboard-admin/pharmacy/godown/godownStockRequest/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Page() {
   const [stockRequest, setStockRequests] = useState([]);
