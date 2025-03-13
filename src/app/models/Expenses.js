ecf6a3ed5dcabfdcac2ec165c97f99fedda194cb
varunvaatsalya <a.varungupta23@gmail.com>
2025-03-14 01:14:26 +0530

    expense default

--- src/app/models/Expenses.js
+++ src/app/models/Expenses.js
@@ -1,27 +1,9 @@
-// import mongoose from "mongoose";
-
-// const expenseSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   amount: { type: Number, required: true },
-//   quantity: { type: Number },
-//   validity: { type: String },
-//   expenseMessage: { type: String },
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// export default mongoose.models.Expense ||
-//   mongoose.model("Expense", expenseSchema);
-
 import mongoose from "mongoose";
 
 const expenseSchema = new mongoose.Schema({
-  category: { type: String, required: true }, //--
   name: { type: String, required: true },
   amount: { type: Number, required: true },
   quantity: { type: Number },
-  units: { type: String, required: true }, //--
-  cost_per_unit: { type: String, required: true }, //--
-
   validity: { type: String },
   expenseMessage: { type: String },
   createdAt: { type: Date, default: Date.now },
@@ -29,3 +11,21 @@ const expenseSchema = new mongoose.Schema({
 
 export default mongoose.models.Expense ||
   mongoose.model("Expense", expenseSchema);
+
+// import mongoose from "mongoose";
+
+// const expenseSchema = new mongoose.Schema({
+//   category: { type: String, required: true }, //--
+//   name: { type: String, required: true },
+//   amount: { type: Number, required: true },
+//   quantity: { type: Number },
+//   units: { type: String, required: true }, //--
+//   cost_per_unit: { type: String, required: true }, //--
+
+//   validity: { type: String },
+//   expenseMessage: { type: String },
+//   createdAt: { type: Date, default: Date.now },
+// });
+
+// export default mongoose.models.Expense ||
+//   mongoose.model("Expense", expenseSchema);
