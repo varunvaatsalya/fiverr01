56ee9add1f91f65d6e57980dd7722f058bfd3d3a
varunvaatsalya <a.varungupta23@gmail.com>
2025-04-23 14:29:56 +0530

    order history

--- src/app/dashboard-stockist/stockOrderHistory/page.js
+++ src/app/dashboard-stockist/stockOrderHistory/page.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../../../../components/Navbar";
-import StockOrderHistory from "../../../../components/StockOrderHistory";
+import Navbar from "../../components/Navbar";
+import StockOrderHistory from "../../components/StockOrderHistory";
 
 function Page() {
   return (
