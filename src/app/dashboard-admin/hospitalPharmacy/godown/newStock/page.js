fbe0d7c6f35e1f73050da120ef5089b3a62cbf5b
varunvaatsalya <a.varungupta23@gmail.com>
2025-07-29 18:48:47 +0530

    checking

--- src/app/dashboard-admin/hospitalPharmacy/godown/newStock/page.js
+++ src/app/dashboard-admin/hospitalPharmacy/godown/newStock/page.js
@@ -1,27 +1,53 @@
 "use client";
-import React, { useEffect, useState } from 'react'
-import Navbar from '@/app/components/Navbar';
-import NewStockForm from '@/app/components/NewStockForm';
-
+import React, { useEffect, useState } from "react";
+import Navbar from "@/app/components/Navbar";
+import NewStockForm from "@/app/components/NewStockForm";
+import { useStockType } from "@/app/context/StockTypeContext";
 
 function Page() {
+  const sectionType = useStockType();
+
   const [medicines, setMedicines] = useState([]);
-  const [ids, setIds] = useState([]);
-  
-    useEffect(() => {
-      fetch("/api/newMedicine?basicInfo=1&sectionType=hospital")
-        .then((res) => res.json())
-        .then((data) => {
-          setMedicines(data.response);
-          setIds(data.ids);
-        });
-    }, []);
+  const [lists, setLists] = useState([]);
+  const [uniqueID, setUniqueID] = useState(null);
+  const [type, setType] = useState("vendor");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let result = await fetch(
+          `/api/newPurchaseInvoice?sourceType=${type}${
+            medicines.length === 0 ? "&medicinesDetails=1" : ""
+          }${
+            !uniqueID ? "&generateNewId=1" : ""
+          }${sectionType === "hospital" ? "&sectionType=hospital" : ""}`
+        );
+        result = await result.json();
+        if (result.success) {
+          setLists(result.response.lists);
+          if(result.response.medicines) setMedicines(result.response.medicines || []);
+          if(result.response.uniqueID) setUniqueID(result.response.uniqueID);
+        } else {
+          setMessage(result.message);
+        }
+      } catch (err) {
+        console.log("error: ", err);
+      }
+    }
+    fetchData();
+  }, [type]);
   return (
-    <div>
+    <div className="bg-white min-h-screen">
       <Navbar route={["Hospital", "GoDown", "New Stock"]} />
-      <NewStockForm medicines={medicines} ids={ids} />
+      <NewStockForm
+        medicines={medicines}
+        lists={lists}
+        type={type}
+        setType={setType}
+        uniqueID={uniqueID}
+      />
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
